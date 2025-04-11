52f864e139af0d406e65c510292807d687e2263c
wook099 <codnrud99@naver.com>
2025-04-11 09:31:10 +0900

    sw

---
+++ src/main/java/hello1/demo/controller/MainController.java
@@ -0,0 +1,17 @@
+package hello1.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hello")
+public class MainController {
+
+    @GetMapping
+    public String intro(Model model){
+        model.addAttribute("greeting","hello jenkins");
+        return "intro";
+    }
+}
---
+++ src/test/java/hello1/demo/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package hello1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
